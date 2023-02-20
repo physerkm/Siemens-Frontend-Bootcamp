a4980425602948376fa64deccb72649ee5d76d18
physerkm <erkam.sis@outlook.com>
2023-02-20 12:09:05 +0300

    yeni notlar eklendi

---
+++ day11/my-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+  // one-way binding
+  name:string = "angular";
+
+  change() {
+    this.name = "angular 15";
+  }
+}
