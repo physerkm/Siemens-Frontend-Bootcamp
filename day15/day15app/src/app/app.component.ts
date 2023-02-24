a6f84de9a0a0074ba11031c8c784a9a8007b6782
physerkm <erkam.sis@outlook.com>
2023-02-24 11:32:47 +0300

    New notes are added.

---
+++ day15/day15app/src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { FakeserviceService } from './fakeservice.service';
+
+interface product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'day15app';
+
+  constructor(private fakeService:FakeserviceService) {
+
+    // this.fakeService.getPostsWithUserId(1).subscribe(x => {
+    //   console.log(x)
+    // })
+
+    //this.fakeService.getPostsWithUserId(1).subscribe(x => {
+    //  console.log(x)
+    // })
+
+    // this.fakeService.UpdatePut({id:1, title:'title 1',body:'body 1', userId:2}).subscribe({
+    //  next:(data)=>console.log(data),
+    //  error:(err)=> console.log(err.message),
+    //  complete:()=>console.log("Completed!")
+      
+    //  })
+
+
+// this.fakeService.SavePostWithError({id:1, title:'title 1',body:'body 1', userId:2}).subscribe({
+// next:(data)=>console.log(data),
+// error:(err)=> console.log(err.message),
+// complete:()=>console.log("tamamalandı.")
+
+// })
+
+// this.fakeService.GetPostsWithHeader().subscribe(x=>{
+//  console.log(x);
+// })
+
+}
+}
