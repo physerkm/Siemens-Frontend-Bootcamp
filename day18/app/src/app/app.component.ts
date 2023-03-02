47b7a8c1a33e0052327ff9238e11fb33f0e425c5
physerkm <erkam.sis@outlook.com>
2023-03-02 08:44:26 +0300

    New notes are added.

---
+++ day18/app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Angular App';
+
+  change() {
+    this.title = 'Angular App 2';
+  }
+}
---
+++ day18/app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostListComponent } from './pages/post/post-list/post-list.component';
+import { PostCreateComponent } from './pages/post/post-create/post-create.component';
+import { PostUpdateComponent } from './pages/post/post-update/post-update.component';
+import { HomeComponent } from './pages/common/home/home.component';
+import { PagenotfoundComponent } from './pages/common/pagenotfound/pagenotfound.component';
+import{HttpClientModule} from '@angular/common/http';
+import { PostDetailComponent } from './pages/post/post-detail/post-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostListComponent,
+    PostCreateComponent,
+    PostUpdateComponent,
+    HomeComponent,
+    PagenotfoundComponent,
+    PostDetailComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule,HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
